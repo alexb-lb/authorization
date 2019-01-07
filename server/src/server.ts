2ac9055f15f6404ecdfe24dfa1b2bde424bc6b27
Alex Bobrenko <oknerbob@gmail.com>
2019-01-07 11:47:59 +0200

    moved to vs code

--- server/src/server.ts
+++ server/src/server.ts
@@ -13,13 +13,13 @@ import initialPlugin from './plugins/initial/initial';
 import Logger from './modules/Logger';
 
 /** Configure environment */
-dotenv.config({path: '.env.development'});
+dotenv.config({ path: '.env.development' });
 
 /** Server config defaulting to NODE_ENV or "development" */
 const server = Hapi.server({
   port: process.env.HTTP_PORT,
   host: process.env.HOST,
-  routes: {files: { relativeTo: path.join(__dirname, '..', 'client','public') }}
+  routes: { files: { relativeTo: path.join(__dirname, '..', 'client', 'public') } }
 });
 
 /** Server init */
@@ -29,12 +29,12 @@ const init = async () => {
   await server.register(initialPlugin);
 
   // static files like images
-  server.route({method: 'GET', path: '/images/{file*}', handler: {directory: { path: './images', listing: true }}});
-  server.route({method: 'GET', path: '/dist/{file*}', handler: {directory: { path: './dist', listing: true }}});
+  server.route({ method: 'GET', path: '/images/{file*}', handler: { directory: { path: './images', listing: true } } });
+  server.route({ method: 'GET', path: '/dist/{file*}', handler: { directory: { path: './dist', listing: true } } });
 
   // use React router instead of server routing for all of the paths
   // server.route({method: 'GET', path: '/{route*}', handler: (request, h) => h.file('./dist/app.html')});
-  server.route({method: 'GET', path: '/{route*}', handler: (request, h) => 'hello world'});
+  server.route({ method: 'GET', path: '/{route*}', handler: (request, h) => 'hello world' });
 
   await server.start();
   Logger.createLog(`Server running at: ${server.info.uri}`);
